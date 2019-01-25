e2551f2bb64ed87b13284c0bfef990636f6ef2a6
Samantha Wong <smiwong920@gmail.com>
2019-01-25 21:55:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function inc(n)  {
 function dec(n)  {
   return (n-=1)
 }
-function makeInt(n,10)  {
-  return parseInt(n)
+function makeInt(n)  {
+  return parseInt(n,10)
 }
 function preserveDecimal(n)  {
   return parseFloat(n)
